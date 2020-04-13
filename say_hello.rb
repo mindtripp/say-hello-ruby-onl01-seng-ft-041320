92683af7a1d696c89ffd8a979af3bef8b11aeba8
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-13 20:13:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,4 @@
-# Build your say_hello method here
-def say_hello
-puts "Gabriela"
+# Build your say_hello method 
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
-
-say_hello("Kent Beck")
-
